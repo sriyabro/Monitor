9bd1980cd4facc640f102516bdac9bd8417e61d8
Rahul Madhuka <adihetti_im17003@stu.kln.ac.lk>
2021-07-09 04:02:34 +0530

    Creating Backend Functions

---
+++ monitor-backend/server.js
@@ -0,0 +1,34 @@
+const express =  require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json({limit: '50mb', extended: true}));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{useNewUrlParser: true , useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open' , () => {
+    console.log("MongoDB database connection established succesfully");
+})
+
+const userRouter = require('./routes/user');
+const sensorRouter = require('./routes/sensor');
+
+
+app.use('/users' , userRouter);
+app.use('/sensors' , sensorRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port : ${port}`);
+});
+
