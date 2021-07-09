dedb378acb3c11462668bb7ce896777da1d87a1d
shaniAbeywick <shaniabeywickrama294@gmail.com>
2021-07-09 14:29:19 +0530

    form validated

---
+++ monitor-frontend/src/components/signup.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import { FormHelperText } from '@material-ui/core'
+import * as Yup from 'yup'
+const Signup = () => {
+    const paperStyle = { padding: 20, width: 300, margin: "0 auto" }
+    const headerStyle = { margin: 0 }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const marginTop = { marginTop: 5 }
+    const initialValues = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        termsAndConditions: false
+    }
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().min(3, "It's too short").required("Required"),
+        email: Yup.string().email("Enter valid email").required("Required"),
+        password: Yup.string().min(8, "Password minimum length should be 8").required("Required"),
+        confirmPassword: Yup.string().oneOf([Yup.ref('password')], "Password not matched").required("Required"),
+        termsAndConditions: Yup.string().oneOf(["true"], "Accept terms & conditions")
+    })
+    const onSubmit = (values, props) => {
+        console.log(values)
+        console.log(props)
+        setTimeout(() => {
+
+            props.resetForm()
+            props.setSubmitting(false)
+        }, 2000)
+    }
+    return (
+        <Grid>
+            <Paper style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}>
+                        <AddCircleOutlineOutlinedIcon />
+                    </Avatar>
+                    <h2 style={headerStyle}>Sign Up</h2>
+                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+                </Grid>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                    {(props) => (
+                        <Form>
+
+                            <Field as={TextField} fullWidth name="name" label='Name'
+                                   placeholder="Enter your name" helperText={<ErrorMessage name="name" />} />
+                            <Field as={TextField} fullWidth name="email" label='Email'
+                                   placeholder="Enter your email" helperText={<ErrorMessage name="email" />} />
+                            <Field as={TextField} fullWidth name='password' type="password"
+                                   label='Password' placeholder="Enter your password"
+                                   helperText={<ErrorMessage name="password" />} />
+                            <Field as={TextField} fullWidth name="confirmPassword" type="password"
+                                   label='Confirm Password' placeholder="Confirm your password"
+                                   helperText={<ErrorMessage name="confirmPassword" />} />
+                            <FormControlLabel
+                                control={<Field as={Checkbox} name="termsAndConditions" />}
+                                label="I accept the terms and conditions."
+                            />
+                            <FormHelperText><ErrorMessage name="termsAndConditions" /></FormHelperText>
+                            <Button type='submit' variant='contained' disabled={props.isSubmitting}
+                                    color='primary'>{props.isSubmitting ? "Loading" : "Sign up"}</Button>
+
+                        </Form>
+                    )}
+                </Formik>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Signup;
