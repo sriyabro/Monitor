3cd94e1e94c6e57f27983d810b31bd74e3e05511
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-09 14:44:51 +0530

    react feather icons added

--- monitor-frontend/src/components/header/Header.js
+++ monitor-frontend/src/components/header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./Header.css";
+import {Button, Col, Row} from "react-bootstrap";
+import {LogOut, User} from 'react-feather';
 
 
 function Header() {
     return (
-        <header>
-
-        </header>
+        <Row className="py-2 header">
+            <Col xs={6} >
+                <h1 className="logo pl-2">Monitor</h1>
+            </Col>
+            <Col xs={6} className="text-right">
+                <span className="mr-3 d-none d-md-inline"><User/> Username </span>
+                <Button className="logout my-2 py-1 px-3 "><label className="mb-1 p-0">Logout</label>&nbsp;&nbsp;<LogOut size={18}/></Button>
+            </Col>
+        </Row>
     );
 }
 
