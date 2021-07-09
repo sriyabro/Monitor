b3f715ba16e14caea5a462f82ca874dacf3a98d4
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-09 09:03:49 +0530

    send alert mail completed in alertUser function

--- monitor-backend/routes/sensor.js
+++ monitor-backend/routes/sensor.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let Sensor = require('../models/sensor.model');
 let User = require('../models/users.model');
+let alertUser = require('../controllers/alert');
 
 
 router.route('/add').post(async (req,res) => {
@@ -11,7 +12,7 @@ router.route('/add').post(async (req,res) => {
         let sensor = await Sensor.findOne({ sensor_name: req.body.sensor_name});
         if(sensor) return res.status(400).send('Sensor Existing')
 
-        let userOb = await User.findById(req.body.sensor_user).populate({path : 'cart_Items.product' , model : 'Product'})   
+        let userOb = await User.findById(req.body.sensor_user);
     
         //Create New User
         const sensor_user = userOb;
@@ -40,15 +41,17 @@ router.route('/add').post(async (req,res) => {
 router.route('/data').post(async(req,res) => {
 
     //let userOb = await User.findById(req.params.id)
-    let sensor = await Sensor.findOne({_id: req.body.sensor_id})
+    let sensor = await Sensor.findOne({_id: req.body.sensor_id}).populate('sensor_user');
+
+    let value = req.body.values;
 
     sensor.sensor_readings.push({
-        values : req.body.values,
+        values : value,
         date_time : Date(req.body.date_time)
     })
 
-    if(sensor.sensor_threshold <= req.body.values){
-        console.log("Danger Detected!!!")
+    if(sensor.sensor_threshold <= value){
+        alertUser(sensor, value);
     }
 
     await sensor.save()
