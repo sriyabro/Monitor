f5d178dbfac1d63a7a143de2ce56d13dc9043b4c
shaniAbeywick <shaniabeywickrama294@gmail.com>
2021-07-09 14:29:19 +0530

    form validated

--- monitor-frontend/src/App.js
+++ monitor-frontend/src/App.js
@@ -3,12 +3,14 @@ import {Container} from "react-bootstrap";
 import "./assets/styles/main.scss"
 import './App.css';
 import Login from "./components/login";
+import Signup from "./components/signup";
+import SignInOutContainer from "./containers";
 
 function App() {
   return (
       <div className="App">
 
-          <Login/>
+          <SignInOutContainer/>
       </div>
 
   );
