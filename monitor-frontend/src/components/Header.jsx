c4dd5b6fbffe2140a36dfd084e78f6ac2c9f41c5
Rahul Madhuka <adihetti_im17003@stu.kln.ac.lk>
2021-07-10 17:42:09 +0530

    Retrieving logged user details using jwt-decode

--- monitor-frontend/src/components/Header.jsx
+++ monitor-frontend/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Button, Col, Row} from "react-bootstrap";
 import {LogOut, User} from 'react-feather';
+import jwtDecode from "jwt-decode";
 
 function Header() {
+
+    const jwt = localStorage.getItem("token");
+    let userName = jwtDecode(jwt).user_Name;
+
     return (
         <Row className="p-3 header">
             <Col xs={6} >
                 <h1 className="logo my-0">Monitor</h1>
             </Col>
             <Col xs={6} className="text-right">
-                <span className="mr-4 d-none d-md-inline user"><User/> Mark</span>
+                <span className="mr-4 d-none d-md-inline user"><User/> {userName}</span>
                 <Button className="logout my-2 py-1 px-3" variant="outline-dark">Logout &nbsp;&nbsp; <LogOut size={18}/></Button>
             </Col>
         </Row>
