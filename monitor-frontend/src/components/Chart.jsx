de5e55620153824bd0b6ada616bab23d5f531a7a
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-11 01:30:49 +0530

    selected sensor data fetched

--- monitor-frontend/src/components/Chart.jsx
+++ monitor-frontend/src/components/Chart.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Line} from "react-chartjs-2";
 import {Col} from "react-bootstrap";
 
 const Chart = ({sensor}) => {
+    const sensor_times = []; //labels
+    const sensor_values = []; //data
+    const [selectedSensorLabel, setSelectedSensorLabel] = useState({label: "No Sensor Selected"});
+
+
+
+    console.log("sensor in chart",sensor);
+   // useEffect(() => {
+   //     sensor.sensor_readings.map((reading) => {
+   //         sensor_times.push(reading.date_time);
+   //         sensor_values.push(reading.values);
+   //     });
+   // }, [sensor]);
+
     const data = {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
         datasets: [
             {
-                label: sensor.label,
+                label: selectedSensorLabel,
                 data: [250, 150, 55, 1, 223, 65],
                 fill: true,
                 backgroundColor: "rgba(75,192,192,0.2)",
