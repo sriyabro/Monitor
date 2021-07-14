d851938d27d0297e67d6b051ecbc0f8097f448dc
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-14 09:37:03 +0530

    sensor component added in sensor list

--- monitor-frontend/src/components/UserProfile.jsx
+++ monitor-frontend/src/components/UserProfile.jsx
@@ -4,6 +4,7 @@ import Header from "./Header";
 import jwtDecode from "jwt-decode";
 import Axios from "axios";
 import {Trash2} from "react-feather";
+import SensorItem from "./SensorItem";
 
 const UserProfile = () => {
     const jwt = localStorage.getItem("token");
@@ -58,28 +59,7 @@ const UserProfile = () => {
                         <Col xs={12}>
                             {sensors.map((sensor) => {
                                 return (
-                                    <Row key={sensor._id} className="sensor my-2 py-2">
-                                        <Col xs={6}>
-                                            <h5>{sensor.sensor_name}</h5>
-                                            <p className="m-0">
-                                                Threshold Value: <span
-                                                className="text-danger">{sensor.sensor_threshold}</span>
-                                            </p>
-                                        </Col>
-                                        <Col xs={5}>
-                                    <span>last reading updated on:
-                                        <p className="m-0">
-                                            {sensor.sensor_readings.length === 0
-                                                ? "No past readings"
-                                                : sensor.sensor_readings[sensor.sensor_readings.length - 1].date_time
-                                            }
-                                        </p>
-                                    </span>
-                                        </Col>
-                                        <Col xs={1} className="delete-col text-danger">
-                                            <Trash2 className="delete" size={20}/>
-                                        </Col>
-                                    </Row>
+                                    <SensorItem sensor={sensor} key={sensor._id}/>
                                 );
                             })}
                         </Col>
@@ -87,7 +67,7 @@ const UserProfile = () => {
                 </Col>
             </Row>
         </React.Fragment>
-    );
+);
 }
 
 export default UserProfile;
