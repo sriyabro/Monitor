a577b1e7ffaf2794fa0b3bbcf6ccc9d5917dbbf8
Sriyanjith Herath <sriyabro@gmail.com>
2021-07-14 09:32:10 +0530

    user profile page added

--- monitor-frontend/src/components/Header.jsx
+++ monitor-frontend/src/components/Header.jsx
@@ -10,19 +10,25 @@ const Header = () => {
     let userName = jwtDecode(jwt).user_Name;
     const history = useHistory();
 
-    function handleLogout(){
+    function handleLogout() {
         localStorage.clear();
         history.push('/')
     }
 
+    const handleUserClicked = () => {
+        history.push('/profile');
+    }
+
     return (
         <Row className="p-2 header">
-            <Col xs={6} >
+            <Col xs={6}>
                 <h1 className="logo my-0 pl-2">Monitor</h1>
             </Col>
             <Col xs={6} className="text-right">
-                <span className="mr-4 d-none d-md-inline user"><User/> {userName}</span>
-                <Button className="logout my-2 py-1 px-3" variant="outline-dark" onClick={handleLogout}>Logout &nbsp;&nbsp; <LogOut size={18}/></Button>
+                <span className="user mr-4 d-none d-md-inline" onClick={handleUserClicked}><User/> {userName}</span>
+                <Button className="logout my-2 py-1 px-3" variant="outline-dark" onClick={handleLogout}>
+                    Logout &nbsp;&nbsp; <LogOut size={18}/>
+                </Button>
             </Col>
         </Row>
     );
